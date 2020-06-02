0523a4f6c54acb00d6166e388ea22c9774b64358
Guilherme Arruda <guilherme_arruda@live.com>
2020-06-02 20:38:35 -0300

    20200602 - Created product profile page

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Topbar from '../../components/Topbar'
 import Products from '../Products'
+import { BrowserRouter } from 'react-router-dom';
+import ProductProfile from '../ProductProfile'
 
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
-      <Topbar />
-      <Products />
+      <BrowserRouter>
+        <Topbar />
+        <Products />
+      </BrowserRouter>
     </div>
   );
 }

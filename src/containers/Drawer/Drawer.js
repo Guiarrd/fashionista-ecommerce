63a5430339fb49ae80081fa9ac1ccf3fd18cf258
Guilherme Arruda <guilherme_arruda@live.com>
2020-06-05 04:23:25 -0300

    20200605 - WIP: Create drawer container

---
+++ src/containers/Drawer/Drawer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Drawer.scss'
+
+const Drawer = () => {
+  return (
+    <div className="drawer">
+      <header className="drawer__header"></header>
+    </div>
+  )
+}
+
+export default Drawer;
